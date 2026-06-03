a5d082915af91f70d5a5ef2ef67b3066345b546f
Michel Klein <michel.kps@outlook.com>
2026-06-03 09:27:24 -0300

    feat: flyway

---
+++ etc/postgres/init-user-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE ordering;
+	CREATE DATABASE ordering_test;
+	CREATE DATABASE billing;
+	CREATE DATABASE billing_test;
+EOSQL
